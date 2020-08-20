bce207a0406805edec227f710e168ff529ed0a04
Mauricio <mba097@gmail.com>
2020-08-19 22:45:44 -0300

    Validação no registro

--- MBLabs/wwwroot/js/site.js
+++ MBLabs/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿$(document).ready(function () {
+    var $seuCampoCpf = $("#Input_CPF");
+    $seuCampoCpf.mask('000.000.000-00', { reverse: true });
+
+    var $seuCampoPhone = $("#Input_Phone");
+    $seuCampoPhone.mask('(00) 00000-0000', { reverse: true });
+});
